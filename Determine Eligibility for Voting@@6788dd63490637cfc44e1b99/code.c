fcfeea50947024fa7850d2b48937a5dd72727497
Tanish Nagpal <nagpaltanish6@gmail.com>
2025-03-07 20:31:14 +0530

    Solution to Determine Eligibility for Voting

--- Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -2,9 +2,9 @@
 int main(){
     int a;
     scanf("%d",&a);
-    if (a <= 18){
+    if (a < 18){
         printf("Not Eligible");
-    } else if (a > 18) {
+    } else if (a >= 18) {
         printf("Eligible");
     } else {
         printf("Gay");
