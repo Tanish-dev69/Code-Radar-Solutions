18f2bdcd5147cf17356fc1453d8219792f390037
Tanish Nagpal <nagpaltanish6@gmail.com>
2025-03-10 12:18:26 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,14 +2,14 @@
 int main(){
     char op;
     int a,b;
-    scanf("%d%d%c",&a,&b,&op);
+    scanf("%d %d %c",&a,&b,&op);
     if (c== '+') {
         printf("%d",a+b);
-    } else if (c== '-'){
+    } else if (c == '-'){
          printf("%d",a-b);
-    } else if (c== '*'){
+    } else if (c == '*'){
          printf("%d",a*b);
-    } else if (c== '/'){
+    } else if (c == '/'){
          printf("%d",a/b);
     } else {printf("error");}
     return 0;
